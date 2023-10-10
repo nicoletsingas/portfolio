e4aeecb0ad66a331a30508be080d6feadebb6842
Nicole Tsingas <nicolecass27@gmail.com>
2023-10-10 11:35:38 -0300

    Criado estrutura do componente projects, HTML e TS

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { projectsData } from 'src/app/projects-data';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-
+  projects = projectsData;
 }
